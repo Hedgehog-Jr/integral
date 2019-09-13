b536eac13dbae5d9da6c499728133fafd11e55c0
Corji4 <beingevilhasaprice>
2019-09-13 16:33:48 +0300

    f(x)
    уточнить значение переменной k

--- f_x.py
+++ f_x.py
@@ -41,14 +41,102 @@ def f10(x):
     return 0.1 * pow(x, 2) / math.log10(x)
 
 
+def f11(x):
+    return 3 * x / pow(1 + pow(x, 3), 0.5)
 
 
+def f12(x):
+    return 1 / (1 + pow(x, 0.5))
 
 
+def f13(x):
+    return math.sin(x) * pow(math.cos(x), 2)
 
 
+def f14(x):
+    return (math.sin(x) + math.cos(x)) / (3 + math.sin(2 * x))
 
 
+def f15(x):
+    return 1 / (math.exp(x) + math.exp(-x))
 
 
+def f16(x):
+    return pow(x - 2, 2/3) / (pow(x - 2, 2/3) + 3)
+
+
+def f17(x):
+    return math.exp(x) * (math.exp(x) - 1) / (math.exp(x) + 3)
+
+
+def f18(x):
+    return x / pow(2 + 4 * x, 0.5)
+
+
+def f19(x):
+    return 1 / (3 + 2 * math.cos(x))
+
+
+def f20(x):
+    return pow(4 - pow(math.sin(x), 2), 0.5) / 2
+
+
+def f21(x):
+    return math.exp(2 * x) / pow(1 - pow(x, 2), 0.5)
+
+
+def f22(x):
+    return 1 / (pow(1 - pow(x, 2), 0.5) * (1 + pow(x, 2)))
+
+
+def f23(x):
+    return math.sin(x) / pow(x, 0.5)
+
+
+def f24(x):
+    return 1 / (pow(x, 0.5) * (math.exp(0.4 * x) + 1.5))
+
+
+def f25(x):
+    return pow(x, 0.5) / math.sin(pow(x, 2))
+
+
+def f26(x):
+    return pow(x, 0.5) / (1 + pow(x, 2))
+
+
+def f27(x):
+    return 1 / pow((1 + pow(x, 2)) * (4 + 3 * pow(x, 2)), 0.5)
+
+
+def f28(x):
+    exit(28)
+
+
+def f29(x, y):
+    return pow(x, 2) / (1 + pow(y, 2))
+
+
+def f30(x, y):
+    return 1 / pow(x + y, 2)
+
+
+def f31(x, y):
+    return pow(x, 2) + 2 * y
+
+
+def f32(x, y):
+    return 4 - pow(x, 2) - pow(y, 2)
+
+
+def f33(x, y):
+    return math.sin(x + y)
+
+
+def f34(x, y):
+    exit(34)
+
+
+def f35(x, y):
+    exit(35)
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import f_x
 
 def main():
     f = [f_x.f1, f_x.f2, f_x.f3, f_x.f4, f_x.f5, f_x.f6, f_x.f7, f_x.f8, f_x.f9, f_x.f10]
-    #f += [f_x.f11, f_x.f12, f_x.f13, f_x.f14, f_x.f15, f_x.f16, f_x.f17, f_x.f18, f_x.f19, f_x.f20]
+    f += [f_x.f11, f_x.f12, f_x.f13, f_x.f14, f_x.f15, f_x.f16, f_x.f17, f_x.f18, f_x.f19, f_x.f20]
     #f += [f_x.f21, f_x.f22, f_x.f23, f_x.f24, f_x.f25, f_x.f26, f_x.f27, f_x.f28, f_x.f29, f_x.f30]
     #f += [f_x.f31, f_x.f32, f_x.f33, f_x.f34, f_x.f35]
     x = float(input("Введите x\n"))

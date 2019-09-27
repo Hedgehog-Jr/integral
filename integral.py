63fbb04970ddc689e0b6d5ec0c75b1ec8f65d218
Corji4 <beingevilhasaprice>
2019-09-27 10:07:25 +0300

    interface

--- integral.py
+++ integral.py
@@ -3,6 +3,7 @@ import copy
 
 
 def trapeze(f, a, b, eps):
+    k = 1
     # вычисление интеграла при n = 2
     n = 2
     x = a
@@ -12,7 +13,9 @@ def trapeze(f, a, b, eps):
         x += h
         i_h += f(x)
     i_h *= h
+    print("\t", k, "\t" * 3, h, "\t" * 3, i_h)
     # вычисление интеграла при n = 4
+    k += 1
     x = a
     n *= 2
     h = (b - a) / n
@@ -21,21 +24,25 @@ def trapeze(f, a, b, eps):
         x += h
         i_h2 += f(x)
     i_h2 *= h
+    print("\t", k, "\t" * 3, h, "\t" * 3, i_h2)
     # проверка на точность
     while math.fabs(i_h - i_h2) > 3 * eps:
         i_h = copy.deepcopy(i_h2)
         x = a
+        n *= 2
         h = (b - a) / n
         i_h2 = (f(a) + f(b)) / 2
         for i in range(1, n):
             x += h
             i_h2 += f(x)
         i_h2 *= h
-
-    return i_h2
+        k += 1
+        print("\t", k, "\t" * 3, h, "\t" * 3, i_h2)
+    # return i_h2
 
 
 def simpson(f, a, b, eps):
+    k = 1
     n = 1
     # вычисление интеграла при n = 1
     x = a
@@ -50,6 +57,7 @@ def simpson(f, a, b, eps):
     x += h
     i_h += f(x)
     i_h *= h / 3
+    print("\t", k, "\t" * 3, h, "\t" * 3, i_h)
     # вычисление интеграла при n = 2
     n *= 2
     x = a
@@ -64,6 +72,8 @@ def simpson(f, a, b, eps):
     x += h
     i_h2 += f(x)
     i_h2 *= h / 3
+    k += 1
+    print("\t", k, "\t" * 3, h, "\t" * 3, i_h2)
     # проверка на точность
     while math.fabs(i_h - i_h2) > 15 * eps:
         i_h = copy.deepcopy(i_h2)
@@ -80,7 +90,9 @@ def simpson(f, a, b, eps):
         x += h
         i_h2 += f(x)
         i_h2 *= h / 3
-    return i_h2
+        k += 1
+        print("\t", k, "\t" * 3, h, "\t" * 3, i_h2)
+    # return i_h2
 
 
 def cubature(f, a, b, c, d, eps):
@@ -102,6 +114,8 @@ def cubature(f, a, b, c, d, eps):
             integral1 += f(x2, y2) + 4 * f(x2_1, y2) + f(x2_2, y2) + 4 * f(x2, y2_1) + 16 * f(x2_1, y2_1)
             integral1 += 4 * f(x2_2, y2_1) + f(x2, y2_2) + 4 * f(x2_1, y2_2) + f(x2_2, y2_2)
     integral1 *= hx * hy / 9
+    k = 1
+    print("\t", k, "\t" * 3, hx, "\t" * 3, hy, "\t" * 3, integral1)
     # вычисление интеграла при n = m = 4
     n *= 2
     m *= 2
@@ -118,6 +132,8 @@ def cubature(f, a, b, c, d, eps):
             integral2 += f(x2, y2) + 4 * f(x2_1, y2) + f(x2_2, y2) + 4 * f(x2, y2_1) + 16 * f(x2_1, y2_1)
             integral2 += 4 * f(x2_2, y2_1) + f(x2, y2_2) + 4 * f(x2_1, y2_2) + f(x2_2, y2_2)
     integral2 *= hx * hy / 9
+    k = 2
+    print("\t", k, "\t" * 3, hx, "\t" * 3, hy, "\t" * 3, integral2)
     # проверка на точность
     while math.fabs(integral1 - integral2) > 15 * eps:
         integral1 = copy.deepcopy(integral2)
@@ -137,5 +153,7 @@ def cubature(f, a, b, c, d, eps):
                 integral2 += f(x2, y2) + 4 * f(x2_1, y2) + f(x2_2, y2) + 4 * f(x2, y2_1) + 16 * f(x2_1, y2_1)
                 integral2 += 4 * f(x2_2, y2_1) + f(x2, y2_2) + 4 * f(x2_1, y2_2) + f(x2_2, y2_2)
         integral2 *= hx * hy / 9
+        k += 1
+        print("\t", k, "\t" * 3, hx, "\t" * 3, hy, "\t" * 3, integral2)
     return integral1
 
